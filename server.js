e343e1e175c926424e45859fa9fc45e9f7eca0e9
jesngyn <54013825+jesngyn@users.noreply.github.com>
2019-09-04 21:58:38 +1000

    asd

--- server.js
+++ server.js
@@ -1,23 +1,41 @@
-let express = require('express');
-let app = express();
+const express = require("express");
+const mongodb = require("mongodb");
+const bodyparser = require('body-parser');
+const morgan = require('morgan');
 
-let db = [];
+//Configure Express
+const app = express()
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
-
 app.use(express.static('images'));
 app.use(express.static('css'));
 app.use(express.static('views'));
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(morgan('common'));
 
-//initialise body parser
-let bodyParser = require('body-parser');
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({
-    extended: false
-}))
-// parse application/json
-app.use(bodyParser.json())
+//Configure MongoDB
+const MongoClient = mongodb.MongoClient;
+
+// Connection URL
+const url = "mongodb://localhost:27017/";
+
+//reference to the database (i.e. collection)
+let db;
+
+//Connect to mongoDB server
+MongoClient.connect(url, { useNewUrlParser: true },
+    function (err, client) {
+        if (err) {
+            console.log("Err  ", err);
+        } else {
+            console.log("Connected successfully to server");
+            db = client.db("week5db");
+        }
+    });
+
+
+//---- Route Handlers
 
 //index
 app.get('/', function (req, res) {
@@ -26,21 +44,33 @@ app.get('/', function (req, res) {
 
 //add task POST req
 app.post('/addtask', function (req, res) {
-    db.push({
-        taskname: req.body.taskname,
-        taskdue: req.body.taskdue,
-        taskdesc: req.body.taskdesc
+    let taskDetails = req.body;
+    //generate random ID to whole number
+    //source: https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
+    let randId = Math.floor(Math.random() * (19999 - 10000 + 1)) + 10000;
+    db.collection('tasks').insertOne({
+        taskid: randId,
+        name: taskDetails.taskname,
+        assignedto: taskDetails.taskassign,
+        duedate: taskDetails.taskdue,
+        taskstatus: taskDetails.taskstatus,
+        description: taskDetails.taskdesc
     });
-    console.log(req.body.taskname);
-    console.log(req.body.taskdue);
-    console.log(req.body.taskdesc);
-    res.render("listtask", {tasks: db});
+    res.redirect('/listall');
 });
 
 //list all GET req
 app.get('/listall', function(req, res) {
-    res.render("listtask", {tasks: db});
-    console.log(db);
+    db.collection('tasks').find({}).toArray(function (err, data) {
+        res.render("listtask", {tasks: data});
+    });
+});
+
+app.post('/deletetaskbyid', function (req, res){
+    let taskDetails = req.body;
+    let filter = { taskid: taskDetails.taskid };
+    db.collection('tasks').deleteOne(filter);
+    res.redirect('/listall');
 });
 
 app.listen(8080);
